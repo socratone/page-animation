085160666ace476c0206970560d0d9005d99a467
socratone <gim2origin@gmail.com>
2020-09-12 23:42:32 +0900

    페이지 애니메이션

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 const TOP_MARGIN = 100;
-const MAX_HEIGHT = 497;
 
 const nextButton = document.getElementById('next-button');
 nextButton.addEventListener('click', function () {
@@ -15,74 +14,88 @@ preButton.addEventListener('click', function () {
 });
 
 function turnNextPage() {
+  let bottomEdgeX = 920;
   let bottomCornerX = 920;
   let bottomCornerY = 582 + TOP_MARGIN;
-  let bottomEdgeX = 920;
-  let topEdgeX = 920;
   let topCornerX = 920;
-  let topCornerY = 0 + TOP_MARGIN;
+  let topCornerY = 582 + TOP_MARGIN;
+  let topEdgeX = 920;
+  let topEdgeY = 582 + TOP_MARGIN;
 
-  let turningSpeed = 3.5;
+  let bottomCornerSpeed = 3.5;
+  let topCornerSpeed = 3.5;
 
   const id = setInterval(function () {
     drawLine({
+      bottomEdgeX,
       bottomCornerX,
       bottomCornerY,
-      bottomEdgeX,
       topCornerX,
       topCornerY,
       topEdgeX,
+      topEdgeY,
     });
 
-    setBottomEdgeX();
-    setTopEdgeX();
-
-    setBottomCornerX();
-    setTopCornerX();
-
-    setCorners();
+    moveBottomCornerLeft();
+    moveBottomEdgeLeft();
+    if (topEdgeY <= 0 + TOP_MARGIN) {
+      moveTopCornerLeft();
+      moveTopEdgeLeft();
+    } else {
+      moveTopsUp();
+    }
 
     if (bottomCornerX < 0) {
       clearInterval(id);
-      trimBottomCorner();
+      trimPage();
     }
   }, 1);
 
-  function setBottomCornerX() {
-    bottomCornerX = bottomCornerX - 5;
-  }
-
-  function setTopCornerX() {
-    topCornerX = topCornerX - 5;
-  }
-
-  function setBottomEdgeX() {
+  function moveBottomEdgeLeft() {
     bottomEdgeX = 920 - (920 - bottomCornerX) / 2;
   }
 
-  function setTopEdgeX() {
-    topEdgeX = 920 - (920 - bottomCornerX) / 2;
+  function moveTopEdgeLeft() {
+    topEdgeX = topEdgeX - 5;
   }
 
-  function setCorners() {
-    // 상승
-    if (bottomCornerX >= 920 / 2) {
-      if (bottomCornerY > MAX_HEIGHT + TOP_MARGIN) {
-        bottomCornerY -= turningSpeed;
-        topCornerY -= turningSpeed;
-
-        turningSpeed *= 0.96;
-      }
-      // 하강
+  function moveTopCornerLeft() {
+    topCornerX = topCornerX - 10; // x축 이동
+    // y축 상승
+    if (topCornerX >= 920 / 2) {
+      topCornerY -= topCornerSpeed;
+      topCornerSpeed *= 0.96;
     } else {
-      bottomCornerY += turningSpeed;
-      topCornerY += turningSpeed;
-
-      turningSpeed *= 1.04;
+      topCornerY += topCornerSpeed;
+      topCornerSpeed *= 1.04;
     }
   }
 
-  function trimBottomCorner() {
+  function moveBottomCornerLeft() {
+    bottomCornerX = bottomCornerX - 5; // x축 이동
+    // y축 상승
+    if (bottomCornerX >= 920 / 2) {
+      bottomCornerY -= bottomCornerSpeed;
+      bottomCornerSpeed *= 0.96;
+      // y축 하강
+    } else {
+      bottomCornerY += bottomCornerSpeed;
+      bottomCornerSpeed *= 1.04;
+    }
+  }
+
+  function moveTopsUp() {
+    const result = topEdgeY - 6;
+    if (result < 0 + TOP_MARGIN) {
+      topEdgeY = 0 + TOP_MARGIN;
+      topCornerY = 0 + TOP_MARGIN;
+    } else {
+      topEdgeY = result;
+      topCornerY = result;
+    }
+  }
+
+  function trimPage() {
     drawLine({
       bottomCornerX: 0,
       bottomCornerY: 582 + TOP_MARGIN,
@@ -90,24 +103,26 @@ function turnNextPage() {
       topCornerX: 0,
       topCornerY: 0 + TOP_MARGIN,
       topEdgeX: 920 / 2,
+      topEdgeY: TOP_MARGIN,
     });
   }
 }
 
 function drawLine({
+  bottomEdgeX,
   bottomCornerX,
   bottomCornerY,
-  bottomEdgeX,
   topCornerX,
   topCornerY,
   topEdgeX,
+  topEdgeY,
 }) {
   ctx.clearRect(0, 0, 920, 582 + TOP_MARGIN);
   ctx.beginPath();
   ctx.moveTo(bottomEdgeX, 582 + TOP_MARGIN);
   ctx.lineTo(bottomCornerX, bottomCornerY);
   ctx.lineTo(topCornerX, topCornerY);
-  ctx.lineTo(topEdgeX, 0 + TOP_MARGIN);
+  ctx.lineTo(topEdgeX, topEdgeY);
   ctx.closePath();
   ctx.stroke();
 }
