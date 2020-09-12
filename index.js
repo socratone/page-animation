893f406b8eb22884d1385876757b5e2baa3c7a92
socratone <gim2origin@gmail.com>
2020-09-13 01:47:03 +0900

    코드 정리

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function PageTurning() {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
 
+  const TURNING_SPEED = 10;
   const BOOK_WIDTH = 920; // 이 값에 따라 나머지 비율이 변한다.
   const BOOK_HEIGHT = Math.round(BOOK_WIDTH * 0.6326);
   const TOP_MARGIN = Math.round(BOOK_WIDTH * 0.1086);
@@ -17,7 +18,29 @@ function PageTurning() {
   const preButton = document.getElementById('pre-button');
   preButton.addEventListener('click', function () {
     console.log('이전 버튼 클릭');
+    animationId && clearInterval(animationId);
+    turnPrePage();
   });
+
+  function drawPageTurning({
+    bottomEdgeX,
+    bottomCornerX,
+    bottomCornerY,
+    topCornerX,
+    topCornerY,
+    topEdgeX,
+    topEdgeY,
+  }) {
+    ctx.clearRect(0, 0, BOOK_WIDTH, BOOK_HEIGHT + TOP_MARGIN);
+    ctx.beginPath();
+    ctx.moveTo(bottomEdgeX, BOOK_HEIGHT + TOP_MARGIN);
+    ctx.lineTo(bottomCornerX, bottomCornerY);
+    ctx.lineTo(topCornerX, topCornerY);
+    ctx.lineTo(topEdgeX, topEdgeY);
+    ctx.closePath();
+    ctx.fillStyle = 'rgb(230, 230, 230)';
+    ctx.fill();
+  }
 
   function turnNextPage() {
     let bottomEdgeX = BOOK_WIDTH;
@@ -57,7 +80,7 @@ function PageTurning() {
         clearInterval(animationId);
         trimPage();
       }
-    }, 10); // 속도 조절
+    }, TURNING_SPEED);
 
     function moveBottomEdgeLeft() {
       const result = BOOK_WIDTH - (BOOK_WIDTH - bottomCornerX) / 2;
@@ -78,32 +101,29 @@ function PageTurning() {
     }
 
     function moveTopCornerLeft() {
-      topCornerX -= 16; // x축 이동
-      // y축 상승
+      topCornerX -= 16; // x축 이동 속도
       if (topCornerX >= BOOK_WIDTH / 2) {
-        topCornerY -= topCornerSpeed;
+        topCornerY -= topCornerSpeed; // y축 상승
         topCornerSpeed *= 0.96;
       } else {
-        topCornerY += topCornerSpeed;
+        topCornerY += topCornerSpeed; // y축 하강
         topCornerSpeed *= 1.04;
       }
     }
 
     function moveBottomCornerLeft() {
-      bottomCornerX -= 10; // x축 이동
-      // y축 상승
+      bottomCornerX -= 10; // x축 이동 속도
       if (bottomCornerX >= BOOK_WIDTH / 2) {
-        bottomCornerY -= bottomCornerSpeed;
+        bottomCornerY -= bottomCornerSpeed; // y축 상승
         bottomCornerSpeed *= 0.96;
-        // y축 하강
       } else {
-        bottomCornerY += bottomCornerSpeed;
+        bottomCornerY += bottomCornerSpeed; // y축 하강
         bottomCornerSpeed *= 1.04;
       }
     }
 
     function moveTopsUp() {
-      const result = topEdgeY - 16; // y축 이동
+      const result = topEdgeY - 16; // y축 이동 속도
       if (result < 0 + TOP_MARGIN) {
         topEdgeY = 0 + TOP_MARGIN;
         topCornerY = 0 + TOP_MARGIN;
@@ -124,26 +144,109 @@ function PageTurning() {
         topEdgeY: TOP_MARGIN,
       });
     }
+  }
 
-    function drawPageTurning({
-      bottomEdgeX,
-      bottomCornerX,
-      bottomCornerY,
-      topCornerX,
-      topCornerY,
-      topEdgeX,
-      topEdgeY,
-    }) {
-      ctx.clearRect(0, 0, BOOK_WIDTH, BOOK_HEIGHT + TOP_MARGIN);
-      ctx.beginPath();
-      ctx.moveTo(bottomEdgeX, BOOK_HEIGHT + TOP_MARGIN);
-      ctx.lineTo(bottomCornerX, bottomCornerY);
-      ctx.lineTo(topCornerX, topCornerY);
-      ctx.lineTo(topEdgeX, topEdgeY);
-      ctx.closePath();
-      ctx.fillStyle = 'rgb(230, 230, 230)';
-      ctx.fill();
-      // ctx.stroke();
+  function turnPrePage() {
+    let bottomEdgeX = 0;
+    let bottomCornerX = 0;
+    let bottomCornerY = BOOK_HEIGHT + TOP_MARGIN;
+    let topCornerX = 0;
+    let topCornerY = BOOK_HEIGHT + TOP_MARGIN;
+    let topEdgeX = 0;
+    let topEdgeY = BOOK_HEIGHT + TOP_MARGIN;
+
+    let bottomCornerSpeed = 3.5;
+    let topCornerSpeed = 3.5;
+
+    animationId = setInterval(function () {
+      drawPageTurning({
+        bottomEdgeX,
+        bottomCornerX,
+        bottomCornerY,
+        topCornerX,
+        topCornerY,
+        topEdgeX,
+        topEdgeY,
+      });
+
+      moveBottomCornerRight();
+      moveBottomEdgeRight();
+      if (topEdgeY <= 0 + TOP_MARGIN) {
+        moveTopCornerRight();
+        moveTopEdgeRight();
+      } else {
+        moveTopsUp();
+      }
+
+      if (bottomCornerX >= BOOK_WIDTH) bottomCornerX = BOOK_WIDTH;
+      if (topCornerX >= BOOK_WIDTH) topCornerX = BOOK_WIDTH;
+      if (bottomCornerX === BOOK_WIDTH && topCornerX === BOOK_WIDTH) {
+        clearInterval(animationId);
+        trimPage();
+      }
+    }, TURNING_SPEED);
+
+    function moveBottomEdgeRight() {
+      const result = bottomCornerX / 2;
+      if (result >= BOOK_WIDTH / 2) {
+        bottomEdgeX = BOOK_WIDTH / 2;
+      } else {
+        bottomEdgeX = result;
+      }
+    }
+
+    function moveTopEdgeRight() {
+      const result = topCornerX / 2;
+      if (result >= BOOK_WIDTH / 2) {
+        topEdgeX = BOOK_WIDTH / 2;
+      } else {
+        topEdgeX = result;
+      }
+    }
+
+    function moveTopCornerRight() {
+      topCornerX += 16; // x축 이동 속도
+      if (topCornerX <= BOOK_WIDTH / 2) {
+        topCornerY -= topCornerSpeed; // y축 상승
+        topCornerSpeed *= 0.96;
+      } else {
+        topCornerY += topCornerSpeed; // y축 하강
+        topCornerSpeed *= 1.04;
+      }
+    }
+
+    function moveBottomCornerRight() {
+      bottomCornerX += 10; // x축 이동 속도
+      if (bottomCornerX <= BOOK_WIDTH / 2) {
+        bottomCornerY -= bottomCornerSpeed; // y축 상승
+        bottomCornerSpeed *= 0.96;
+      } else {
+        bottomCornerY += bottomCornerSpeed; // y축 하강
+        bottomCornerSpeed *= 1.04;
+      }
+    }
+
+    function moveTopsUp() {
+      const result = topEdgeY - 16; // y축 이동 속도
+      if (result < 0 + TOP_MARGIN) {
+        topEdgeY = 0 + TOP_MARGIN;
+        topCornerY = 0 + TOP_MARGIN;
+      } else {
+        topEdgeY = result;
+        topCornerY = result;
+      }
+    }
+
+    function trimPage() {
+      drawPageTurning({
+        bottomCornerX: BOOK_WIDTH,
+        bottomCornerY: BOOK_HEIGHT + TOP_MARGIN,
+        bottomEdgeX: BOOK_WIDTH / 2,
+        topCornerX: BOOK_WIDTH,
+        topCornerY: 0 + TOP_MARGIN,
+        topEdgeX: BOOK_WIDTH / 2,
+        topEdgeY: TOP_MARGIN,
+      });
     }
   }
 }
