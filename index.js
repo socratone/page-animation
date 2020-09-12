5ea9ec15f68453d71a9cf9b05ca5682a7b6442c8
socratone <gim2origin@gmail.com>
2020-09-13 00:19:04 +0900

    width에 따라 애니메이션이 비례되도록 설정

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const TOP_MARGIN = 100;
+const BOOK_WIDTH = 920; // 이 값에 따라 나머지 비율이 변한다.
+const BOOK_HEIGHT = Math.round(BOOK_WIDTH * 0.6326);
+const TOP_MARGIN = Math.round(BOOK_WIDTH * 0.1086);
 
 const nextButton = document.getElementById('next-button');
 nextButton.addEventListener('click', function () {
@@ -14,13 +16,13 @@ preButton.addEventListener('click', function () {
 });
 
 function turnNextPage() {
-  let bottomEdgeX = 920;
-  let bottomCornerX = 920;
-  let bottomCornerY = 582 + TOP_MARGIN;
-  let topCornerX = 920;
-  let topCornerY = 582 + TOP_MARGIN;
-  let topEdgeX = 920;
-  let topEdgeY = 582 + TOP_MARGIN;
+  let bottomEdgeX = BOOK_WIDTH;
+  let bottomCornerX = BOOK_WIDTH;
+  let bottomCornerY = BOOK_HEIGHT + TOP_MARGIN;
+  let topCornerX = BOOK_WIDTH;
+  let topCornerY = BOOK_HEIGHT + TOP_MARGIN;
+  let topEdgeX = BOOK_WIDTH;
+  let topEdgeY = BOOK_HEIGHT + TOP_MARGIN;
 
   let bottomCornerSpeed = 3.5;
   let topCornerSpeed = 3.5;
@@ -52,7 +54,7 @@ function turnNextPage() {
   }, 1);
 
   function moveBottomEdgeLeft() {
-    bottomEdgeX = 920 - (920 - bottomCornerX) / 2;
+    bottomEdgeX = BOOK_WIDTH - (BOOK_WIDTH - bottomCornerX) / 2;
   }
 
   function moveTopEdgeLeft() {
@@ -62,7 +64,7 @@ function turnNextPage() {
   function moveTopCornerLeft() {
     topCornerX = topCornerX - 10; // x축 이동
     // y축 상승
-    if (topCornerX >= 920 / 2) {
+    if (topCornerX >= BOOK_WIDTH / 2) {
       topCornerY -= topCornerSpeed;
       topCornerSpeed *= 0.96;
     } else {
@@ -74,7 +76,7 @@ function turnNextPage() {
   function moveBottomCornerLeft() {
     bottomCornerX = bottomCornerX - 5; // x축 이동
     // y축 상승
-    if (bottomCornerX >= 920 / 2) {
+    if (bottomCornerX >= BOOK_WIDTH / 2) {
       bottomCornerY -= bottomCornerSpeed;
       bottomCornerSpeed *= 0.96;
       // y축 하강
@@ -98,11 +100,11 @@ function turnNextPage() {
   function trimPage() {
     drawPageTurning({
       bottomCornerX: 0,
-      bottomCornerY: 582 + TOP_MARGIN,
-      bottomEdgeX: 920 / 2,
+      bottomCornerY: BOOK_HEIGHT + TOP_MARGIN,
+      bottomEdgeX: BOOK_WIDTH / 2,
       topCornerX: 0,
       topCornerY: 0 + TOP_MARGIN,
-      topEdgeX: 920 / 2,
+      topEdgeX: BOOK_WIDTH / 2,
       topEdgeY: TOP_MARGIN,
     });
   }
@@ -117,14 +119,14 @@ function drawPageTurning({
   topEdgeX,
   topEdgeY,
 }) {
-  ctx.clearRect(0, 0, 920, 582 + TOP_MARGIN);
+  ctx.clearRect(0, 0, BOOK_WIDTH, BOOK_HEIGHT + TOP_MARGIN);
   ctx.beginPath();
-  ctx.moveTo(bottomEdgeX, 582 + TOP_MARGIN);
+  ctx.moveTo(bottomEdgeX, BOOK_HEIGHT + TOP_MARGIN);
   ctx.lineTo(bottomCornerX, bottomCornerY);
   ctx.lineTo(topCornerX, topCornerY);
   ctx.lineTo(topEdgeX, topEdgeY);
   ctx.closePath();
-  ctx.fillStyle = 'gray';
+  ctx.fillStyle = 'rgb(230, 230, 230)';
   ctx.fill();
   // ctx.stroke();
 }
